47778704512f56bdcc972d347c661d7e22882ab7
SAMUEL <samuel@macbook-pro-de-samuel.local>
2018-11-20 20:34:08 -0300

    Se agrega el botón del menú.

--- iOSAvanzado-Lab4/TableViewController.m
+++ iOSAvanzado-Lab4/TableViewController.m
@@ -8,9 +8,15 @@
 
 #import "TableViewController.h"
 #import "TableViewCell.h"
+#import "Servicios/ProductServices.h"
+#import "Servicios/model/Product.h"
+#import "Servicios/WebImageOperations.h"
+#import "TableViewController.h"
 
 @interface TableViewController ()
 @property (strong, nonatomic) IBOutlet UITableView *tableViwe;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) TableViewController* tableViewController;
 
 @end
 
@@ -18,6 +24,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.activityIndicator setHidden:YES];
+}
+
+- (IBAction)recargarTapped:(id)sender {
+    [self.tableViewController.products removeAllObjects];
+    [self.tableViewController reloadTable];
+    
+    [self reloadFromService];
+}
+
+- (void)reloadFromService {
+    [self.activityIndicator setHidden:NO];
+    
+    [ProductServices getProductsOnCompletion:^(id response, ServiceError *error)
+     {
+         if (response) {
+             [self.tableViewController reloadTable];
+             self.tableViewController.products = (NSMutableArray<Product*>*) response;
+             [self.activityIndicator setHidden:YES];
+         }
+         if (error){
+             NSLog(@"%@", error.reason);
+         }
+     }];
 }
 
 #pragma mark - Table view data source
--- iOSAvanzado-Lab4/ViewController.m
+++ iOSAvanzado-Lab4/ViewController.m
@@ -7,44 +7,15 @@
 //
 
 #import "ViewController.h"
-#import "Servicios/ProductServices.h"
-#import "Servicios/model/Product.h"
-#import "Servicios/WebImageOperations.h"
-#import "TableViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
-@property (weak, nonatomic) TableViewController* tableViewController;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.activityIndicator setHidden:YES];
-}
-
-- (IBAction)recargarTapped:(id)sender {
-    [self.tableViewController.products removeAllObjects];
-    [self.tableViewController reloadTable];
-    
-    [self reloadFromService];
-}
-
-- (void)reloadFromService {
-    [self.activityIndicator setHidden:NO];
-    
-    [ProductServices getProductsOnCompletion:^(id response, ServiceError *error)
-     {
-         if (response) {
-             [self.tableViewController reloadTable];
-             self.tableViewController.products = (NSMutableArray<Product*>*) response;
-             [self.activityIndicator setHidden:YES];
-         }
-         if (error){
-             NSLog(@"%@", error.reason);
-         }
-     }];
 }
 
 @end
